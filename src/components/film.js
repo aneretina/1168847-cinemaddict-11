3290e53937e0a967475ce80722bbeafa8562cf8b
Anna Neretinaя <neretinaaa@gmail.com>
2020-04-07 22:25:28 +0200

    отступы подправила

--- src/components/film.js
+++ src/components/film.js
@@ -7,8 +7,8 @@ export const createFilmTemplate = () => {
 export const createFilmListTemplate = () => {
   return (
     `<section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container"></div>
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container"></div>
     </section>`
   );
 };
--- src/components/topRated.js
+++ src/components/topRated.js
@@ -2,8 +2,8 @@
 export const createTopRatedTemplate = () => {
   return (
     `<section class="films-list--extra">
-    <h2 class="films-list__title">Top rated</h2>
-    <div class="films-list__container"></div>
+      <h2 class="films-list__title">Top rated</h2>
+      <div class="films-list__container"></div>
     </section>`
   );
 };
