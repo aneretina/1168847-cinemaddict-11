0182f549d4dbc05fd8a3f4dc944dd585ad0400b2
Anna Neretinaя <neretinaaa@gmail.com>
2020-04-26 22:45:40 +0200

    var 2 of pageController

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,11 +1,15 @@
+import FilmComponent from "../components/film.js";
 import FilmCardComponent from "../components/filmCard.js";
 import TopRatedComponent from "../components/topRated.js";
 import MostCommentedComponent from "../components/mostCommented.js";
 import PopupComponent from "../components/popup.js";
 import {EXTRA_FILM_CARDS, FILM_CARDS_PER_ROW, FILM_CARDS_BY_BUTTON, ESC_KEY} from "../const.js";
 import {render, RenderPosition} from "../utils/render";
-import {body, filmComponent, main} from "../main";
 import {generatedFilms} from "../mock/generateFilmCards";
+
+const main = document.querySelector(`.main`);
+const body = document.querySelector(`body`);
+
 
 const renderFilm = (container, film, position) => {
   const popupComponent = new PopupComponent(film);
@@ -41,48 +45,60 @@ const renderFilm = (container, film, position) => {
   });
 };
 
-const renderFilmCollections = (container, films) => {
-  const topRatedComponent = new TopRatedComponent();
-  const mostCommentedComponent = new MostCommentedComponent();
-  const filmsListContainer = filmComponent.getElement().querySelector(`.films-list__container`);
-  const topRatedFilmsContainer = topRatedComponent.getElement().querySelector(`.films-list__container`);
-  const mostCommentedFilmsContainer = mostCommentedComponent.getElement().querySelector(`.films-list__container`);
-  let showingFilmsCount = FILM_CARDS_PER_ROW;
-
-  render(main, filmComponent, RenderPosition.BEFOREEND);
-
-  films.slice(0, showingFilmsCount)
-  .forEach((film) => renderFilm(filmsListContainer, film, RenderPosition.BEFOREEND));
-
-  const mostCommentedFilms = generatedFilms.slice().sort((a, b) => a.comments.length >= b.comments.length ? -1 : 1);
-  const topRatedFilms = generatedFilms.slice().sort((a, b) => a.rating > b.rating ? -1 : 1);
-  render(filmComponent.getElement(), topRatedComponent, RenderPosition.BEFOREEND);
-  render(filmComponent.getElement(), mostCommentedComponent, RenderPosition.BEFOREEND);
-
-  topRatedFilms.slice(0, EXTRA_FILM_CARDS)
-   .forEach((film) => renderFilm(topRatedFilmsContainer, film, RenderPosition.BEFOREEND));
-
-  mostCommentedFilms.slice(0, EXTRA_FILM_CARDS)
-  .forEach((film) => renderFilm(mostCommentedFilmsContainer, film, RenderPosition.BEFOREEND));
-
-  filmComponent.setClickShowMoreBtnHandler(() => {
-    const prevFilmsCount = showingFilmsCount;
-    showingFilmsCount = showingFilmsCount + FILM_CARDS_BY_BUTTON;
-
-    films.slice(prevFilmsCount, showingFilmsCount).forEach((film) => renderFilm(filmsListContainer, film, RenderPosition.BEFOREEND));
-
-    if (showingFilmsCount >= generatedFilms.length) {
-      filmComponent.removeShowMoreBtn();
-    }
-  });
+const renderNoFilmList = (container) => {
+  render(main, container, RenderPosition.BEFOREEND);
 };
 
 export default class PageController {
   constructor(container) {
     this._container = container;
+    this._topRatedComponent = new TopRatedComponent();
+    this._mostCommentedComponent = new MostCommentedComponent();
+    this._filmComponent = new FilmComponent();
   }
 
   render(films) {
-    renderFilmCollections(this._container, films);
+    const container = this._container;
+
+    render(container, this._filmComponent, RenderPosition.BEFOREEND);
+
+    let showingFilmsCount = FILM_CARDS_PER_ROW;
+
+    films.slice(0, showingFilmsCount)
+      .forEach((film) => renderFilm(container, film, RenderPosition.BEFOREEND));
+
+    const topRatedFilmsContainer = this._topRatedComponent.getElement().querySelector(`.films-list__container`);
+    const mostCommentedFilmsContainer = this._mostCommentedComponent.getElement().querySelector(`.films-list__container`);
+
+    const mostCommentedFilms = films.slice().sort((a, b) => a.comments.length >= b.comments.length ? -1 : 1);
+    const topRatedFilms = films.slice().sort((a, b) => a.rating > b.rating ? -1 : 1);
+    render(container, this._topRatedComponent, RenderPosition.BEFOREEND);
+    render(container, this._mostCommentedComponent, RenderPosition.BEFOREEND);
+
+    topRatedFilms.slice(0, EXTRA_FILM_CARDS)
+       .forEach((film) => renderFilm(topRatedFilmsContainer, film, RenderPosition.BEFOREEND));
+
+    mostCommentedFilms.slice(0, EXTRA_FILM_CARDS)
+      .forEach((film) => renderFilm(mostCommentedFilmsContainer, film, RenderPosition.BEFOREEND));
+
+    this._filmComponent.setClickShowMoreBtnHandler(() => {
+      const prevFilmsCount = showingFilmsCount;
+      console.log(showingFilmsCount)
+      showingFilmsCount = showingFilmsCount + FILM_CARDS_BY_BUTTON;
+
+      films.slice(prevFilmsCount, showingFilmsCount).forEach((film) => renderFilm(container, film, RenderPosition.BEFOREEND));
+
+      if (showingFilmsCount >= generatedFilms.length) {
+        this._filmComponent.removeShowMoreBtn();
+      }
+    });
+
+    let filmComponent;
+    if (films.length === 0) {
+      filmComponent = new FilmComponent(`There are no movies in our database`, true);
+      renderNoFilmList(filmComponent);
+    } else {
+      filmComponent = new FilmComponent(`All movies. Upcoming`, false);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 
 import ProfileComponent from "./components/profile.js";
 import MenuComponent from "./components/menu.js";
-import FilmComponent from "./components/film.js";
 import StatisticsComponent from "./components/statistics.js";
 import {generatedFilms} from "./mock/generateFilmCards";
 import {generateMenu} from "./mock/generateMenu";
 import {render, RenderPosition} from "./utils/render";
 import PageControllerComponent from "./controllers/pageController";
+import FilmComponent from "./components/film.js";
 
 const header = document.querySelector(`.header`);
-export const main = document.querySelector(`.main`);
+const main = document.querySelector(`.main`);
 const footer = document.querySelector(`.footer`);
 const footerStatistics = footer.querySelector(`.footer__statistics`);
-export const body = document.querySelector(`body`);
 
-export let filmComponent;
-
-const renderNoFilmList = (container) => {
-  render(main, container, RenderPosition.BEFOREEND);
-};
+const filmComponent = new FilmComponent();
+const pageController = new PageControllerComponent(filmComponent.getElement());
 
 const menu = generateMenu(generatedFilms);
 
@@ -26,15 +22,6 @@ render(header, new ProfileComponent(), RenderPosition.BEFOREEND);
 render(main, new MenuComponent(menu), RenderPosition.BEFOREEND);
 render(footerStatistics, new StatisticsComponent(), RenderPosition.BEFOREEND);
 
+pageController.render(generatedFilms);
 
-if (generatedFilms.length === 0) {
-  filmComponent = new FilmComponent(`There are no movies in our database`, true);
-  renderNoFilmList(filmComponent);
-} else {
-  filmComponent = new FilmComponent(`All movies. Upcoming`, false);
-
-  const filmElement = filmComponent.getElement();
-  const pageController = new PageControllerComponent(filmElement);
-  pageController.render(generatedFilms);
-}
 
